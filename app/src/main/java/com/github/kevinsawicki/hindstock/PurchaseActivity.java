5e3fbf533279e8e67fa2650d0651575730ff9ae9
Kevin Sawicki <kevinsawicki@gmail.com>
2012-10-05 08:38:49 -0700

    Use FloatMath.floor instead of Math.floor

--- app/src/main/java/com/github/kevinsawicki/hindstock/PurchaseActivity.java
+++ app/src/main/java/com/github/kevinsawicki/hindstock/PurchaseActivity.java
@@ -31,6 +31,7 @@ import android.content.DialogInterface.OnDismissListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -406,7 +407,7 @@ public class PurchaseActivity extends SherlockActivity {
 
 	private void displayQuote(final Quote quote) {
 		float netAmount = quote.getNet();
-		double dollars = Math.floor(Math.abs(netAmount) + 0.5F);
+		float dollars = FloatMath.floor(Math.abs(netAmount) + 0.5F);
 		int percentage = Math.round(quote.getRate());
 		percentage = Math.abs(percentage);
 		StringBuilder netTextValue = new StringBuilder();
